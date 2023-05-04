f3badf3c7313d71c96f16cedaeb56dc274624bda
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-05-04 16:03:56 +0900

    Update PageHeader.js

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -32,7 +32,7 @@ export default function PageHeader() {
       <div className="squares square7" />
       <Container>
         <div className="content-center brand">
-          <h1 className="h1-seo">충남상성고 외국어과</h1>
+          <h2 className="h1-seo">충남상성고 외국어과</h1>
           <h3 className="d-none d-sm-block">
             A beautiful Design System for Bootstrap 4 (reactstrap) and React.
             It's Free and Open Source.
