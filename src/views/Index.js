28628831593f61a91d06b805c6e01d00d3bfed7e
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-05-04 19:44:36 +0900

    Update Index.js

--- src/views/Index.js
+++ src/views/Index.js
@@ -23,17 +23,18 @@ import PageHeader from "components/PageHeader/PageHeader.js";
 import Footer from "components/Footer/Footer.js";
 
 // sections for this page/view
-import Basics from "views/IndexSections/Basics.js";
+import Notifications from "views/IndexSections/Notifications.js";
+//import Basics from "views/IndexSections/Basics.js";
 import Navbars from "views/IndexSections/Navbars.js";
 import Tabs from "views/IndexSections/Tabs.js";
-import Pagination from "views/IndexSections/Pagination.js";
-import Notifications from "views/IndexSections/Notifications.js";
+//import Pagination from "views/IndexSections/Pagination.js";
+
 import Typography from "views/IndexSections/Typography.js";
 import JavaScript from "views/IndexSections/JavaScript.js";
 import NucleoIcons from "views/IndexSections/NucleoIcons.js";
-import Signup from "views/IndexSections/Signup.js";
-import Examples from "views/IndexSections/Examples.js";
-import Download from "views/IndexSections/Download.js";
+//import Signup from "views/IndexSections/Signup.js";
+//import Examples from "views/IndexSections/Examples.js";
+//import Download from "views/IndexSections/Download.js";
 
 export default function Index() {
   React.useEffect(() => {
