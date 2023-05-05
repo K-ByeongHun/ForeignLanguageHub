3f92906336930ba65dbc1e0bad4f2a60bd254ce9
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-05-05 19:43:47 +0900

    Update Tabs.js

--- src/views/IndexSections/Tabs.js
+++ src/views/IndexSections/Tabs.js
@@ -60,7 +60,7 @@ export default function Tabs() {
                       onClick={(e) => setTextTabs(4)}
                       href="#pablo"
                     >
-                      Profile
+                     게임방법
                     </NavLink>
                   </NavItem>
                   <NavItem>
@@ -71,7 +71,7 @@ export default function Tabs() {
                       onClick={(e) => setTextTabs(5)}
                       href="#pablo"
                     >
-                      Settings
+                      게임예시
                     </NavLink>
                   </NavItem>
                   <NavItem>
@@ -91,27 +91,46 @@ export default function Tabs() {
                 <TabContent className="tab-space" activeTab={"link" + textTabs}>
                   <TabPane tabId="link4">
                     <p>
-                      These cases are perfectly simple and easy to distinguish.
-                      In a free hour, when our power of choice is untrammelled
-                      and when nothing prevents our being able to do what we
-                      like best, every pleasure is to be welcomed and every pain
-                      avoided. <br />
-                      But in certain circumstances and owing to the claims of
-                      duty or the obligations of business it will frequently
-                      occur that pleasures
+                      6번의 시도 안에 오늘의 단어을 알아내세요. 각각의 시도는 5글자의 영어 단어로 이루어 집니다. 
+                      추측이 맞는지 확인하려면 엔터를 입력하세요. 
+                      추측한 단어가 얼마나 답에 근접했는지는 이후 각 타일의 색으로 표시됩니다.
+                    </p>
+                    <p>
+                      만일 타일이 초록색으로 표시된다면:
+                    </p>
+                    <p>
+                      - 표시된 글자가 오늘의 단어에 존재한다.
+                    </p>
+                    <p>
+                      - 표시된 글자가 정확히 같은 위치에 위치한다.
+                    </p>
+                    <p>
+                      만일 타일이 노란색으로 표시된다면:
+                    </p>
+                    <p>
+                      표시된 글자가 오늘의 단어에 존재한다.
+                    </p>
+                    <p>
+                      하지만 정확히 같은 위치에 있지는 않다.
                     </p>
                   </TabPane>
                   <TabPane tabId="link5">
                     <p>
-                      I will be the leader of a company that ends up being worth
-                      billions of dollars, because I got the answers. I
-                      understand culture. I am the nucleus. I think that’s a
-                      responsibility that I have, to push possibilities, to show
-                      people, this is the level that things could be at. I think
-                      that’s a responsibility that I have, to push
-                      possibilities, to show people, this is the level that
-                      things could be at.
+                     예를 들어, 맞취야 하는 단어(워들)가 WORLD라고 가정하자.
                     </p>
+                    <p>
+                     이때, 플레이어의 추측이 WEARY일 경우 다음과 같은 결과가 나타난다.
+                    </p>
+                    <p>
+                     - W: WORLD에 포함된 글자이며, 두 단어에서 위치가 일치한다. 따라서 초록색으로 표시된다.
+                    </p>
+                    <p>
+                     - R: WORLD에 포함된 글자이지만 위치가 일치하지 않는다. 따라서 노색으로 표시된다.
+                    </p>
+                    <p>
+                     그 외 나머지 글자들(E, A, and Y): WORLD에 전혀 존재하지 않는 글자들이다. 따라서 전부 회색으로 표시된다.
+                    </p>
+                 
                   </TabPane>
                   <TabPane tabId="link6">
                     <p>
