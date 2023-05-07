70ca934c375bb896ccc80d853fafa94d29065533
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-05-07 11:20:27 +0900

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -44,25 +44,12 @@ export default function Footer() {
                   Home
                 </NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink to="/landing-page" tag={Link}>
-                  Landing
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/register-page" tag={Link}>
-                  Register
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/profile-page" tag={Link}>
-                  Profile
-                </NavLink>
-              </NavItem>
+              
             </Nav>
           </Col>
           <Col md="3">
             <Nav>
+            
               <NavItem>
                 <NavLink href="">
                   Global Ambassador
@@ -73,58 +60,11 @@ export default function Footer() {
                   8기 안철민 제작
                 </NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink href="">
-                  Blog
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="">
-                  License
-                </NavLink>
-              </NavItem>
+             
             </Nav>
           </Col>
           <Col md="3">
-            <h3 className="title">Follow us:</h3>
-            <div className="btn-wrapper profile">
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://twitter.com/creativetim"
-                id="tooltip622135962"
-                target="_blank"
-              >
-                <i className="fab fa-twitter" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip622135962">
-                Follow us
-              </UncontrolledTooltip>
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://www.facebook.com/creativetim"
-                id="tooltip230450801"
-                target="_blank"
-              >
-                <i className="fab fa-facebook-square" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip230450801">
-                Like us
-              </UncontrolledTooltip>
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://dribbble.com/creativetim"
-                id="tooltip318450378"
-                target="_blank"
-              >
-                <i className="fab fa-dribbble" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip318450378">
-                Follow us
-              </UncontrolledTooltip>
-            </div>
+            
           </Col>
         </Row>
       </Container>
