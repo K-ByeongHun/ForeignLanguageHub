db38b1c462ab88a4364d2343270ca798a7b4ab7a
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-05-08 22:43:44 +0900

    Update Notifications.js

--- src/views/IndexSections/Notifications.js
+++ src/views/IndexSections/Notifications.js
@@ -46,12 +46,12 @@ export default function Notifications() {
   
         <UncontrolledAlert className="alert-with-icon" color="warning">
           <span data-notify="icon" className="tim-icons icon-bulb-63" />
-          <span>
+          <span style="text-align : center;">
             <b></b>
               <img
                 className="img-fluid rounded shadow"
                 src={require("assets/img/p1.jpg")}
-                style={{ width: "150px" }}
+                style={{ width: 100% }}
               />
           </span>
         </UncontrolledAlert>
