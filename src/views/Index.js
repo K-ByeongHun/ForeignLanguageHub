7290d31ece6b77071e15703ccfc90a83354435cc
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-05-09 23:33:03 +0900

    Update Index.js

--- src/views/Index.js
+++ src/views/Index.js
@@ -45,18 +45,7 @@ import ReactGA from "react-ga";
 
 export default function Index() {
   
-  const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
-  ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
-  ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
-  
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+
 
  
   document.oncontextmenu = new Function ('return false'); //block right-click
