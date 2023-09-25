2a8cbdde5f2d9f01048e54201f3d6ad7972be65d
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-09-25 10:44:05 +0900

    Update Download.js

--- src/views/IndexSections/Download.js
+++ src/views/IndexSections/Download.js
@@ -79,11 +79,11 @@ export default function Download() {
           <p data-ke-size="size16">&nbsp;</p>
           <p data-ke-size="size16">&nbsp;</p>
                     
-          <div className="text-center" lg="8" md="12">
+          <Col className="text-center" lg="8" md="12">
               <Button color="primary" to="register-page" tag={Link}>
                 이뿌영 학습 영상 바로가기
               </Button>
-            </div>
+          </Col>
                     
           <Col className="text-center" lg="8" md="12">
             <Button
