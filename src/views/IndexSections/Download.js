b396b2a25849d43e06afbeed4f37e9fdfc81ae29
proxima1619 <100192520+proxima1619@users.noreply.github.com>
2023-10-10 19:56:25 +0900

    Update Download.js

--- src/views/IndexSections/Download.js
+++ src/views/IndexSections/Download.js
@@ -113,5 +113,67 @@ export default function Download() {
         </Row>   
       </Container>
     </div>
+
+
+                
+    <div
+      className="section section-download"
+      data-background-color="black"
+      id="download-section"
+    >
+      <img alt="..." className="path" src={require("assets/img/path1.png")} />
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col className="text-center" lg="8" md="12">
+            <h2 className="title">
+              제2회 영어 말하기 Challenge
+            </h2>
+            <h4 className="description">
+              
+            </h4>
+          </Col>
+          
+          <Col sm="6">
+              
+          </Col>
+          <p data-ke-size="size16">&nbsp;</p>
+          <p data-ke-size="size16">&nbsp;</p>
+          <Col sm="6">
+              <iframe width="100%" height="215px" src="https://www.youtube.com/embed/WQWqXZ07Vrg" 
+                  frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+                  encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+              </iframe>
+            </Col>
+          <p data-ke-size="size16">&nbsp;</p>
+          <p data-ke-size="size16">&nbsp;</p>
+          <p data-ke-size="size16">&nbsp;</p>
+                    
+          <Col className="text-center" lg="8" md="12">
+            <Button
+              className="btn-round"
+              color="info"
+              href="https://www.youtube.com/watch?v=WQWqXZ07Vrg"
+              role="button"
+              size="lg"
+            >
+              영말챌 연습문제 바로가기
+            </Button>
+          </Col>
+                
+          <Col className="text-center" lg="8" md="12">
+            <Button
+              className="btn-round"
+              color="info"
+              href="https://student.cnsa.hs.kr/login/"
+              role="button"
+              size="lg"
+            >
+              영말챌 신청 바로가기
+            </Button>
+          </Col>
+                
+        </Row>   
+      </Container>
+    </div>
   );
 }
