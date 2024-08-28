71e0837f223a0f5810e31d3ee248033f8d456f9a
K-ByeongHun <kangbh013@gmail.com>
2024-08-28 17:05:49 +0900

    정리

--- src/views/Index.js
+++ src/views/Index.js
@@ -6,31 +6,32 @@ import PageHeader from "components/PageHeader/PageHeader.js";
 import Footer from "components/Footer/Footer.js";
 
 // sections for this page/view
-import Notifications from "views/IndexSections/Notifications.js";
+
+// import Notifications from "views/IndexSections/Notifications.js";
 import ESC from "views/IndexSections/EnglishSpeakingChallenge";
-import EBY from "views/IndexSections/bbooyoung";
+// import EBY from "views/IndexSections/bbooyoung";
 import CJT from "views/IndexSections/ChinaJapanTravel";
 
 import Navbars from "views/IndexSections/Navbars.js";
 import WordGame from "views/IndexSections/WordGame";
 
 import Typography from "views/IndexSections/Typography.js";
-import Modals from "views/IndexSections/Modals";
-import NucleoIcons from "views/IndexSections/NucleoIcons.js";
+// import Modals from "views/IndexSections/Modals";
+// import NucleoIcons from "views/IndexSections/NucleoIcons.js";
 
-import Basics from "views/IndexSections/Basics.js";
+// import Basics from "views/IndexSections/Basics.js";
 import Plan from "views/IndexSections/Plan";
-import Signup from "views/IndexSections/Signup.js";
-import Examples from "views/IndexSections/Examples.js";
-import Download from "views/IndexSections/Download.js";
+// import Signup from "views/IndexSections/Signup.js";
+// import Examples from "views/IndexSections/Examples.js";
+// import Download from "views/IndexSections/Download.js";
 
 
 
 export default function Index() {
 
-  document.oncontextmenu = new Function ('return false'); //block right-click
-  document.ondragstart = new Function ('return false'); //block drag and drop
-  document.onselectstart = new Function ('return false'); //block text select
+  // document.oncontextmenu = new Function ('return false'); //block right-click
+  // document.ondragstart = new Function ('return false'); //block drag and drop
+  // document.onselectstart = new Function ('return false'); //block text select
   document.body.style.MozUserSelect = 'none'; //block text select on miscellaneous browsers
 
 
@@ -88,8 +89,8 @@ export default function Index() {
           <Examples /> */}
           <CJT/>
           <ESC/>
-          <EBY/>
-          <Modals />
+          {/* <EBY/> */}
+          {/* <Modals /> */}
           <Navbars />
           <WordGame />
           <Plan />
--- src/views/IndexSections/EnglishSpeakingChallenge.js
+++ src/views/IndexSections/EnglishSpeakingChallenge.js
@@ -15,7 +15,7 @@ export default function Download() {
         <Row className="justify-content-md-center">
           <Col className="text-center" lg="8" md="12">
             <h2 className="title">
-              [신청중] 제2회 영어 말하기 Challenge
+              [Coming Soon]2024 제2회 영어 말하기 Challenge [2024.10.31 EP1]
             </h2>
             <h4 className="description">
               
