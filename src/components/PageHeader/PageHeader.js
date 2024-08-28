1e479e052a7b9d443bddb939aeebd3dfd59ede8c
K-ByeongHun <kangbh013@gmail.com>
2024-08-28 15:41:29 +0900

    Change Fonts

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "assets/css/new.css";
 
 // reactstrap components
 import { Container } from "reactstrap";
@@ -17,12 +18,14 @@ export default function PageHeader() {
       <div className="content-center brand">
           <h1 className="h1-seo" >CNSA</h1>
           <h1 className="h1-seo" >외국어과</h1>
-          <h2 className="d-none d-sm-block">
-            Welcome to the CNSA Foreign Language Hub
-          </h2>
-          <h4 className="d-none d-sm-block">
-          You Can Taste a lot of Foreign Festivals in CNSA!
-          </h4>
+          <div className="EnglishTitle">
+            <h2 className="d-none d-sm-block">
+              Welcome to the CNSA Foreign Language Hub
+            </h2>
+            <h4 className="d-none d-sm-block">
+            You Can Taste a lot of Foreign Festivals in CNSA!
+            </h4>
+          </div>
         </div>
       </Container>
     </div>
